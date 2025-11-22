211f632bf189237a9dd6000a014fa24cab46dcf6
ssreddy1989 <ssreddyfeb17@gmail.com>
2025-11-22 09:06:29 +0530

    Initial COmmit

---
+++ medusa-config.ts
@@ -0,0 +1,18 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+	  host: process.env.HOST || "0.0.0.0", // <--- allow external access
+      port: Number(process.env.PORT) || 9000,
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
+  }
+})
